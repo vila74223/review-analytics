d1e571857af066bdeca65b5b82e778d09b9b38cd
vila74223 <vilahe74223@gmail.com>
2020-09-07 22:01:33 +0900

    rivised syntax

--- reviews.py
+++ reviews.py
@@ -7,22 +7,23 @@ with open('reviews.txt','r') as f:
 		count += 1
 		#length += len(data[count]) (wrong)
 		if count % 1000 == 0:
-			print("There are", len(data) "reviews in total.")
+			print(len(data))
+print("There are", len(data), "reviews in total.")
 #avr = length/count (wrong)
 #prit(avr) (wrong)
 length = 0
 for d in data:
 	length = length + len(d)
-	print("The average length of each review is", length/len(data) ".")
+print("The average length of each review is", length/len(data),".")
 
 new = []
 for d in data:
 	if len(d) < 100:
 		new.append(d)
-print("There are", len(new) "reviews longer than 100 characters.")
+print("There are", len(new), "reviews longer than 100 characters.")
 
 good = []
 for d in data:
-	if good in d:
+	if 'good' in d:
 		good.append(d)
-print("There are", len(good) "reviews mentioned good.")
+print("There are", len(good), "reviews mentioned good.")
