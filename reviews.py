3412947d4de036c911a2563bf3b32f93a6639010
vila74223 <vilahe74223@gmail.com>
2020-09-07 21:41:48 +0900

    number of the review

---
+++ reviews.py
@@ -0,0 +1,13 @@
+data = []
+count = 0
+#length = 0 (wrong)
+with open('reviews.txt','r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		#length += len(data[count]) (wrong)
+		if count % 1000 == 0:
+			print(len(data))
+#avr = length/count (wrong)
+#prit(avr) (wrong)
+
